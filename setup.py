9810dd1be46be2c97d1f7f2400ee8bf9a9fee99f
Joris Roels <jorisro@cn1385.irc.ugent.be>
2020-03-27 17:48:24 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neuralnets",
-    version="1.0.8.7",
+    version="1.0.8.8",
     author="Joris Roels",
     author_email="jorisb.roels@ugent.be",
     description="A library, based on PyTorch, that implements basic neural network algorithms and useful functions",
