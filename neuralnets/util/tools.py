359e919f0b8f2d7f216eb9e982dd88c3858ec9f9
Joris Roels <joris.roels@ugent.vib.be>
2020-04-02 10:41:25 +0200

    code was not fully deterministic

--- neuralnets/util/tools.py
+++ neuralnets/util/tools.py
@@ -136,6 +136,8 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def module_to_device(module, device):
