0316c05a58d00764bff12e2c6820c0f237e5bbaa
Joris Roels <joris.roels@ugent.vib.be>
2020-10-30 11:30:56 +0100

    more efficient metric computing

--- neuralnets/util/metrics.py
+++ neuralnets/util/metrics.py
@@ -2,6 +2,49 @@ import numpy as np
 from scipy.spatial.distance import directed_hausdorff
 
 
+def _jaccard_part_metrics(y_true, y_pred, w=None, max_elements=1000000):
+    """
+    computes the confusion matrix metrics in a memory efficient way
+
+    :param y_true: (N1, N2, ...) array of the true labels
+    :param y_pred: (N1, N2, ...) array of the predictions (either probs or binary)
+    :param w: (N1, N2, ...) masking array
+    :param max_elements: maximum number of elements for block based computing
+    :return: the Jaccard index, accuracy, balanced accuracy, precision, recall and f1-score
+    """
+
+    # flatten, apply masking if necessary and binarize
+    y_true = y_true.flatten()
+    y_pred = y_pred.flatten()
+    if w is not None:
+        w = w.flatten()
+        y_true = y_true[w]
+        y_pred = y_pred[w]
+    y_true = y_true.astype('bool')
+    y_pred = y_pred.astype('bool')
+
+    # compute metrics
+    intersection = 0
+    true_total = 0
+    pred_total = 0
+    n_blocks = y_true.size // max_elements
+    for n in range(n_blocks):
+        # find start and stopping point
+        start = max_elements * n
+        stop = np.minimum(max_elements * (n+1), y_true.size)
+
+        # block to process
+        y_true_block = y_true[start:stop]
+        y_pred_block = y_pred[start:stop]
+
+        # processing
+        intersection += np.sum(y_true_block * y_pred_block)
+        true_total += np.sum(y_true_block)
+        pred_total += np.sum(y_pred_block)
+
+    return intersection, true_total, pred_total
+
+
 def jaccard(y_true, y_pred, w=None):
     """
     Jaccard index between two segmentations
@@ -12,19 +55,9 @@ def jaccard(y_true, y_pred, w=None):
     :return: the Jaccard index
     """
 
-    # check mask
-    if w is None:
-        w = np.ones_like(y_true, dtype='bool')
-    y_true = y_true[w]
-    y_pred = y_pred[w]
-
-    # binarize
-    y_true = (y_true > 0.5).astype('int')
-    y_pred = (y_pred > 0.5).astype('int')
-
     # compute jaccard score
-    intersection = np.sum(y_true * y_pred)
-    union = np.sum(y_true) + np.sum(y_pred) - intersection
+    intersection, true_total, pred_total = _jaccard_part_metrics(y_true, y_pred, w=w)
+    union = true_total + pred_total - intersection
 
     return (intersection + 1) / (union + 1)
 
@@ -44,6 +77,51 @@ def dice(y_true, y_pred, w=None):
     return 2 * j / (1 + j)
 
 
+def _conf_matrix_metrics(y_true, y_pred, w=None, max_elements=1000000):
+    """
+    computes the confusion matrix metrics in a memory efficient way
+
+    :param y_true: (N1, N2, ...) array of the true labels
+    :param y_pred: (N1, N2, ...) array of the predictions (either probs or binary)
+    :param w: (N1, N2, ...) masking array
+    :param max_elements: maximum number of elements for block based computing
+    :return: the Jaccard index, accuracy, balanced accuracy, precision, recall and f1-score
+    """
+
+    # flatten, apply masking if necessary and binarize
+    y_true = y_true.flatten()
+    y_pred = y_pred.flatten()
+    if w is not None:
+        w = w.flatten()
+        y_true = y_true[w]
+        y_pred = y_pred[w]
+    y_true = y_true.astype('bool')
+    y_pred = y_pred.astype('bool')
+
+    # compute metrics
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    n_blocks = y_true.size // max_elements
+    for n in range(n_blocks):
+        # find start and stopping point
+        start = max_elements * n
+        stop = np.minimum(max_elements * (n+1), y_true.size)
+
+        # block to process
+        y_true_block = y_true[start:stop]
+        y_pred_block = y_pred[start:stop]
+
+        # processing
+        tp += np.sum(y_true_block * y_pred_block)
+        tn += np.sum((1 - y_true_block) * (1 - y_pred_block))
+        fp += np.sum((1 - y_true_block) * y_pred_block)
+        fn += np.sum(y_true_block * (1 - y_pred_block))
+
+    return tp, tn, fp, fn
+
+
 def accuracy_metrics(y_true, y_pred, w=None):
     """
     Accuracy metrics between two segmentations (accuracy, balanced accuracy, precision, recall and f1-score)
@@ -51,24 +129,11 @@ def accuracy_metrics(y_true, y_pred, w=None):
     :param y_true: (N1, N2, ...) array of the true labels
     :param y_pred: (N1, N2, ...) array of the predictions (either probs or binary)
     :param w: (N1, N2, ...) masking array
-    :return: the Jaccard index
+    :return: the accuracy, balanced accuracy, precision, recall and f1-score
     """
 
-    # check mask
-    if w is None:
-        w = np.ones_like(y_true, dtype='bool')
-    y_true = y_true[w]
-    y_pred = y_pred[w]
-
-    # binarize
-    y_true = (y_true > 0.5).astype('int')
-    y_pred = (y_pred > 0.5).astype('int')
-
     # compute accuracy metrics
-    tp = np.sum(y_true * y_pred)
-    tn = np.sum((1 - y_true) * (1 - y_pred))
-    fp = np.sum((1 - y_true) * y_pred)
-    fn = np.sum(y_true * (1 - y_pred))
+    tp, tn, fp, fn = _conf_matrix_metrics(y_true, y_pred, w=w)
     total = tp + tn + fp + fn
     accuracy = (tp + tn + 1) / (total + 1)
     recall = (tp + 1) / (tp + fn + 1)
